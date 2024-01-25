477ca5b3dcb8ba23c0399ef744d3447e0afa22f9
alex <al3xback@gmail.com>
2024-01-25 16:25:49 +0700

    restructured html

--- test/main.js
+++ test/main.js
@@ -43,10 +43,12 @@ describe('DOM', () => {
 	});
 
 	it("should have a title element that contains 'Reliable, efficient delivery' word", () => {
-		const sectionTitleEl = document.querySelector('.section__title');
-		const sectionTitle = sectionTitleEl.textContent.trim();
+		const cardsSummaryTitleEl = document.querySelector(
+			'.cards-summary__title'
+		);
+		const cardsSummaryTitle = cardsSummaryTitleEl.textContent.trim();
 
-		sectionTitle.should.include('Reliable, efficient delivery');
+		cardsSummaryTitle.should.include('Reliable, efficient delivery');
 	});
 
 	it('should have two children inside of the section element', () => {
